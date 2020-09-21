3410225fe4d92bafac6b8ead0a3e7f3886aee34e
Ricky Bobby <richard.pajewski@gmail.com>
2020-09-21 13:30:38 -0500

    minor refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,30 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// Assign a variable to the fuction responsible for creating a new task
-var createTaskHandler = function(event) {
+// Accept the form data, store it as an object, then send it to a function
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // Package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    // Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+// Create div and add html for each new task added
+var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
 // Add event listener to task button to create a new task
-formEl.addEventListener("submit", createTaskHandler);
-
+formEl.addEventListener("submit", taskFormHandler);
