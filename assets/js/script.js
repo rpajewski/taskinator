0031bfd875967c3c6ae0a1a94f2de23472bb754c
Ricky Bobby <richard.pajewski@gmail.com>
2020-09-21 08:50:00 -0500

    updated js for form event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // Taskinator - Task Manager Application
 
 // Assign a variables to DOM objects
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Assign a variable to the fuction responsible for creating a new task
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -13,5 +14,5 @@ var createTaskHandler = function() {
 };
 
 // Add event listener to task button to create a new task
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
